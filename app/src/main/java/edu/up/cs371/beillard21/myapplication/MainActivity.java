1305f9c5021886c634aac62013cd7cf102ac6946
beillard21 <beillard21@up.edu>
2018-08-29 18:59:51 -0700

    Second commit

--- app/src/main/java/edu/up/cs371/beillard21/myapplication/MainActivity.java
+++ app/src/main/java/edu/up/cs371/beillard21/myapplication/MainActivity.java
@@ -4,6 +4,7 @@ package edu.up.cs371.beillard21.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+//modified version for commit 2
 public class MainActivity extends AppCompatActivity {
 
     @Override
